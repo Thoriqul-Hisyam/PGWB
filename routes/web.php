93bd23d6fff5bae21bdd9ae1214f1ee87a777b03
samsamsam97 <thoriqulh12@gmail.com>
2021-10-21 07:59:37 +0700

    Tugas 2 : Base admin template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+Route::get('master.app',function() {
+    return view('dashboard');
 });
